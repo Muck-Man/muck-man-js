c0e5271b7a3c561b280f2ea5897a44d6a26a5f1a
Dan <dansoul.cake@gmail.com>
2018-04-09 12:39:32 -0400

    removed a . in the run.js require

---
+++ run.js
@@ -0,0 +1,18 @@
+const MuckClient = require('./bot/MuckClient.js');
+
+const client = new MuckClient({
+	client: {
+		token: '', //discord token
+		cache: {messages: {expire: 30}},
+		gateway: {loadAllMembers: true}
+	},
+	commandClient: {
+		prefixes: ['.m', '.muck'],
+		prefixSpace: true,
+		mentions: true
+	},
+	token: '', //muck api token
+	path: './bot/Commands'
+});
+
+client.run().catch(console.error);
